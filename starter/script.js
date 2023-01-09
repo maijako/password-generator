ed67bdf29f5ca26c08dbe6370efe07056ef28128
ensignjava <109529105+maijako@users.noreply.github.com>
2023-01-09 11:05:45 +0000

    added a condition to stop password function when user character preference is reached

--- starter/script.js
+++ starter/script.js
@@ -88,25 +88,6 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
-//----------------------------------------------------------------------------
-//----------------------------------------------------------------------------
-
-//A variable to store user input for password length
-var passLength = prompt("How long would you like your password to be?");
-
-//A while loop to continue prompting user to enter a value until it is done
-while (passLength == null) {
-  alert("You can't leave the field blank! Please put in a number");
-  passLength = prompt("Please input a number between 10 and 64!");
-}
-//If user does enter a value, run a while loop until the value entered meets acceptance criteria 
-if (passLength !== null) {
-  while (isNaN(passLength) || passLength==="" || passLength < 10 || passLength > 64){
-  passLength = prompt("Please input a number between 10 and 64!");
-  }
-}
-//changing passLength value type back to number and storing it in a new variable
-var targetLength = Number(passLength);
 
 //Variables to  store user character type choices
 var lowerPass, upperPass, numPass, specialPass;
@@ -114,74 +95,87 @@ var lowerPass, upperPass, numPass, specialPass;
 //A variable to store the concatenated array data for password based on user choice
 var userPassArr = "";
 
-//Variables to store user choices of special characters to include, stored in a function
-function getPasswordOptions() {
-  lowerPass = confirm("Include lowercase letters?");
-  // if (lowerPass == true) {
-  //   userPassArr += lowerCasedCharacters;
-  // }
-  upperPass = confirm("Include uppercase letters?");
-  // if (upperPass == true) {
-  //   userPassArr += upperCasedCharacters;
-  // }
-  numPass = confirm("Include numbers?");
-  // if (numPass == true) {
-  //   userPassArr += numericCharacters;
-  // }
-  specialPass = confirm("Include special characters?");
-  // if (specialPass == true){
-  //   userPassArr += specialCharacters;
-  // }
-}
-getPasswordOptions();
+//A variable to store user input for password length
 
-//A while loop to run the function above until the user selects at least one condition
-while ((lowerPass == false) && (upperPass == false) && (numPass == false) && (specialPass == false)) {
-  alert("Please select at least one condition!");
+  var passLength = prompt("How long would you like your password to be?");
+
+  //A while loop to continue prompting user to enter a value until it is done
+  while (passLength == null) {
+    alert("You can't leave the field blank! Please put in a number");
+    passLength = prompt("Please input a number between 10 and 64!");
+  }
+  //If user does enter a value, run a while loop until the value entered meets acceptance criteria 
+  if (passLength !== null) {
+    while (isNaN(passLength) || passLength === "" || passLength < 10 || passLength > 64) {
+      passLength = prompt("Please input a number between 10 and 64!");
+    }
+  }
+
+  //Variables to store user choices of special characters to include, stored in a function
+  function getPasswordOptions() {
+    lowerPass = confirm("Include lowercase letters?");
+    upperPass = confirm("Include uppercase letters?");
+    numPass = confirm("Include numbers?");
+    specialPass = confirm("Include special characters?");
+  }
   getPasswordOptions();
-}
+
+  //A while loop to run the function above until the user selects at least one condition
+  while ((lowerPass == false) && (upperPass == false) && (numPass == false) && (specialPass == false)) {
+    alert("Please select at least one condition!");
+    getPasswordOptions();
+  }
+
+
+//changing passLength value type back to number and storing it in a new variable
+var targetLength = Number(passLength);
+
 
 //Function for getting a random element from an array
 function getRandom(arr) {
-  return arr[Math.floor(Math.random()*arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
-getRandom(lowerCasedCharacters);
-getRandom(upperCasedCharacters)
-getRandom(numericCharacters);
-getRandom(specialCharacters);
 
 //storing random characters in userPassArr variable based on user choices
 function constructPassword() {
 
-      if (lowerPass == true) {
-        userPassArr += getRandom(lowerCasedCharacters);
-      }
-      if (upperPass == true) {
-        userPassArr += getRandom(upperCasedCharacters);
-      }
-      if (numPass == true) {
-        userPassArr += getRandom(numericCharacters);
-      }
-      if (specialPass == true) {
-        userPassArr += getRandom(specialCharacters);
-      }
-      
-    }
+  if (lowerPass == true && userPassArr.length < targetLength) {
+    userPassArr += getRandom(lowerCasedCharacters);
+  }
+  if (upperPass == true && userPassArr.length < targetLength) {
+    userPassArr += getRandom(upperCasedCharacters);
+  }
+  if (numPass == true && userPassArr.length < targetLength) {
+    userPassArr += getRandom(numericCharacters);
+  }
+  if (specialPass == true && userPassArr.length < targetLength) {
+    userPassArr += getRandom(specialCharacters);
+  }
+}
 
 // Function to generate password with user input
 function generatePassword() {
-  while (userPassArr.length < targetLength){
+  while (userPassArr.length < targetLength) {
     constructPassword();
   }
-
 }
 
 generatePassword();
 
+// }
+//   var options = {
+//     "passLength": passLength, 
+//     "lowerPass": lowerPass,
+//     "upperPass": upperPass,
+//     "numPass": numPass,
+//     "specialPass": specialPass
+//   }
+//   return options;
 
 console.log(userPassArr);
 console.log(targetLength);
 console.log(userPassArr.length)
+
 
 // randomize characters in the generated password
 
